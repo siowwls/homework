5c34aceb9f59af2e301ceaa9738c5b9f8012f067
Katya Vinogradova <katyavinogradova@mail.ru>
2024-04-22 18:54:55 +0300

    небольшие изменения

--- test/tests.py
+++ test/tests.py
@@ -1,5 +1,5 @@
-from src.masks import mask_number_card, mask_count
+from src.masks import mask_count, mask_number_card
 
-print(mask_number_card(("7000792289606361")))
+print(mask_number_card("7000792289606361"))
 
 print(mask_count("73654108430135874305"))
