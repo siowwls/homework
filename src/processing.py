96c3693da70024b24d2c0d1ff4526f09cb97c1b3
Katya Vinogradova <katyavinogradova@mail.ru>
2024-04-24 12:19:55 +0300

    small fix add

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,18 @@
-def updated_list(roster: list[dict], key_up: str = "EXECUTED") -> list[dict]:
+def updated_list(data: list[dict], keyword: str = "EXECUTED") -> list[dict]:
     """
     функция принимает на вход список словарей и значение для ключа state и возвращает новый спсиок, содержащий
     только те словари, у которых ключ state содержит переданное в функцию значение
     """
-    new_list = []
-    for item in roster:
-        if item["state"] == key_up:
-            new_list.append(item)
-    return new_list
+    result = []
+    for item in data:
+        if item["state"] == keyword:
+            result.append(item)
+    return result
 
 
-def list_sorting(roster: list[dict], change: bool = True) -> list[dict]:
+def list_sorting(data: list[dict], reversed_order: bool = True) -> list[dict]:
     """
     функция принимает на вход писок словарей и возвращает новый список, в котором исходные словари
-    отсортированы по убыванию даты
+    отсортированы по убыванию даты"
     """
-    new_updated_list = sorted(roster, key=lambda x: x["data"], reverse=change)
-    return new_updated_list
+    return sorted(data, key=lambda x: x["date"], reverse=reversed_order)
