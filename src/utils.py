3fefdaabf5b62f3b59003ced46f8e805d1dd252e
Katya Vinogradova <katyavinogradova@mail.ru>
2024-06-06 13:17:16 +0300

    fix code

--- src/utils.py
+++ src/utils.py
@@ -23,13 +23,16 @@ def open_transactions(filename: str) -> Any:
         return data
     except FileNotFoundError:
         print(f"File {filename} not found.")
+        return []
     except PermissionError:
         print(f"You don't have permission to read file {filename}.")
+        return []
     except json.JSONDecodeError:
         print("Invalid JSON data.")
+        return []
 
 
-print(open_transactions(filename))
+# print(open_transactions(filename))
 
 
 def found_currency(currency: Any) -> Any:
@@ -42,31 +45,28 @@ def found_currency(currency: Any) -> Any:
     return result
 
 
-print(found_currency("USD"))
+# print(found_currency("USD"))
 
 
-def sum_transactions(transactions: list) -> float:
+def sum_transactions(transactions: Any) -> float:
     """
     Функция, которая принимает на вход транзакцию и возвращает сумму транзакции в рублях, возвращает тип float
     """
     all = 0.0
-    for transaction in transactions:
-        if transaction["operationAmount"]["currency"]["code"] == "RUB":
-            all += float(transaction["operationAmount"]["amount"])
-        elif transaction["operationAmount"]["currency"]["code"] == "USD":
-            all += float(transaction["operationAmount"]["amount"] * found_currency("USD"))
-        elif transaction["operationAmount"]["currency"]["code"] == "EUR":
-            all += float(transaction["operationAmount"]["amount"] * found_currency("EUR"))
+    if transactions.get("operationAmount", {}).get("currency", {}).get("code") == "RUB":
+        all += float(transactions["operationAmount"]["amount"])
+    elif transactions.get("operationAmount", {}).get("currency", {}).get("code") == "USD":
+        all += float(transactions["operationAmount"]["amount"]) * found_currency("USD")
+    elif transactions.get("operationAmount", {}).get("currency", {}).get("code") == "EUR":
+        all += float(transactions["operationAmount"]["amount"]) * found_currency("EUR")
     return all
 
 
-transactions = [
-    {
-        "id": 41428829,
-        "state": "EXECUTED",
-        "date": "2019-07-03T18:35:29.512364",
-        "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "RUB"}},
-    }
-]
+transactions = {
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+}
 
 print(sum_transactions(transactions))
